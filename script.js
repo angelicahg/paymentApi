700146a9fcc23660b653bbff5f10250eac68a7bf
Angelica Herrera G <87252525+angelicahg@users.noreply.github.com>
2022-10-05 18:18:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,204 @@
-var alumnos = []
+require("colors");
+const inquirer = require("inquirer");
 
-for (let index = 0; index < 5 ; index++) {
+
+let alumnos = []
+const preguntas = [
+  {
+    type: "list",
+    name: "opcion",
+    message: "¡Bienvenido!¿Que quiere Hacer?",
+    choices: [
+      {
+        value: "1",
+        name: "1.Ver estudiantes"
+      },
+      {
+        value: "2",
+        name: "2.Registro de un nuevo estudiante"
+      },
+      {
+        value: "3",
+        name: "3.Consultar estudiante"
+      },
+      {
+        value: "4",
+        name: "Salir"
+      },
+    ]
+  }
+];
+const consultMenu = [
+  {
+    type: "list",
+    name: "opcion",
+    message: "¡Bienvenido!¿Que quiere Hacer?",
+    choices: [
+      {
+        value: "1",
+        name: "1.Editar estudiante"
+      },
+      {
+        value: "2",
+        name: "2.Eliminar estudiante"
+      },
+      {
+        value: "3",
+        name: "3.Regresar"
+      },
+    ]
+  }
+];
+
+const inquirerMenu = async (menu) => {
+  console.log("==================".blue);
+  console.log("=======Menú=======".green);
+  console.log("==================\n".blue);
+
+  const { opcion } = await inquirer.prompt(menu);
+  return opcion;
+}
+const pause = async () => {
+  const question = [
+    {
+      type: "input",
+      name: "enter",
+      message: "Presione Enter para continuar"
+    }
+  ]
+  await inquirer.prompt(question);
+  console.clear();
+}
+const leerInput = async (message, type) => {
+  const nuevosDtos = [
+    {
+      type,
+      name: "datosAlumnos",
+      message,
+      validate(value) {
+        if (value.length === 0) {
+          return "ingrese un valor"
+        }
+        return true;
+      }
+    }
+  ];
+  const { datosAlumnos } = await inquirer.prompt(nuevosDtos)
+  return datosAlumnos;
+}
+const valueAlumn = async (message, type) => {
+  const nuevosDtos = [
+    {
+      type,
+      name: "datosAlumnos",
+      message,
+      validate(value) {
+        if (alumnos.some(data => data.code === value)) {
+          return "El codigo ya esta registrado,intente nuevamente"
+        }
+        if (value.length === 0) {
+          return "ingrese un valor"
+        }
+        return true;
+      }
+    }
+  ];
+  const { datosAlumnos } = await inquirer.prompt(nuevosDtos)
+  return datosAlumnos;
+}
+function seeAlumn(alumnos) {
+  if (alumnos.length === 0) {
+    console.log("No se encuentran estudiantes inscritos")
+  }
+  else {
+    console.log(alumnos)
+  }
+}
+//Agregar estudiantes
+async function newAlumn() {
+  let newAlumn = {};
+  newAlumn.code= await valueAlumn("codigo", "input");
+  newAlumn.nombre = await leerInput("Nombre");
+  newAlumn.apellido = await leerInput("Apellido", "string");
+  newAlumn.examen = parseFloat(await leerInput("Nota examen", "number"));
+  newAlumn.taller = parseFloat(await leerInput("Nota taller", "number"));
+  newAlumn.quiz = parseFloat(await leerInput("Nota quiz", "number"));
+  newAlumn.notaFinal = newAlumn.examen * 0.5 + newAlumn.taller * 0.3 + newAlumn.quiz * 0.2;
+  alumnos.push(newAlumn);
+  console.table(newAlumn);
+  console.log("El estudiante quedo agregado");
+}
+//editar alumnos 
+async function editAlumn(alumno) {
   
-    
-    var nombre=prompt("nombre estudiante")
-    var apellido=prompt("apellido estudiante")
-    var codigo=prompt("codigo estudiante")
-
-    var nota1=prompt("nota1") 
-    var nota2=prompt("nota2")
-    var nota3=prompt("nota3")
-
-
-    alumnos.push({nombre,apellido,codigo,nota1,nota2,nota3})
-    console.log(alumnos);
+  alumno.code = alumno.code;
+  alumno.nombre = await leerInput("Nombre", "input");
+  alumno.apellido = await leerInput("Apellido", "input");
+  alumno.taller = parseFloat(await leerInput("Nota taller", "input"));
+  alumno.quiz = parseFloat(await leerInput("Nota quiz", "input"));
+  alumno.notaFinal = alumno.examen * 0.5 + alumno.taller * 0.3 + alumno.quiz * 0.2;
+  let newAlumns =[];
+  alumnos.map(a =>{
+    a.code === alumno.code ? newAlumns.push(alumno): newAlumns.push(a)
+   })
+  alumnos = newAlumns;
+  // alumnos.push(editAlumn);
+  // console.table(editAlumn);
+  console.log("Estudiante editado");
+}
+//Eliminar Alumno 
+function deleteAlumn(consulta) {
+  alumnos = alumnos.filter(data => (data.code === consulta) ? false : true);
+  console.log("el estudiante fue eliminado")
 }
 
-const getAlumnoporCodigo = (codigo) =>{
-	let alumno = alumnos.filter(item => Number(item.codigo) === codigo);
-	console.log(alumno);
-	if (alumno.length > 0 ) {
-		return alumno;
-	}
-	else {
-		return "el usuario no existe en la base de datos"
-	}
+// 
+
+//menu 
+async function main() {
+  let opt = "";
+  do {
+    opt = await inquirerMenu(preguntas);
+    console.log(opt);
+    switch (opt) {
+      case "1":
+        seeAlumn(alumnos);
+        break;
+      case "2":
+        await newAlumn();
+        break;
+      case "3":
+        let nuevo = await leerInput("Ingresar el codigo o apellido del estudiante")
+        let obtAlumno = alumnos.filter(data => (data.apellido === nuevo || data.code === nuevo));
+        if (obtAlumno.length === 0) {
+          console.log("No hay concidencia con los datos registrados")
+          break;
+        }else {
+          console.table(obtAlumno);
+        }
+        let index = "";
+        let obtIndex = await leerInput("Ingrese numero del estudiante", "input");
+        let selectAlumn = obtAlumno[obtIndex];
+        console.table(selectAlumn);
+        
+          index = await inquirerMenu(consultMenu);
+          switch (index) {
+            case "1":
+              await editAlumn(selectAlumn);
+              break;
+            case "2":
+              deleteAlumn(selectAlumn.code);
+              break;
+            case "3":
+              console.log("Volver al menú");
+              break;
+          }
+          await pause()
+        break;
+      case "4":
+        console.log("Hasta luego");
+    }await pause();
+  } while (opt !== "4");
 }
 
-	const obtAlumnoporApellido = (tipo) =>{
-		if (typeof tipo === "string") {
-			
-			let alumno = alumnos.filter(item => item.apellido == tipo);
-			if (alumno.length === 1 ) {
-				return alumno;
-			}
-			else if (alumno.length > 1 ){
-			alert(alumno.map(item => `${item.nombre} ${item.apellido} ${item.codigo}` ));
-			let elegirNuevoEtudiante = prompt("digite el codigo del estudiante a consultar")
-			return getAlumnoporCodigo(elegirNuevoEtudiante );
-			}
-	
-			else {
-				return "el usuario no existe en la base de datos_"
-			}
-		}
-		else if (typeof tipo === "number") {
-			console.log("aqui estoy");
-			return getAlumnoporCodigo(tipo)
-			
-		} 
-	
-}
-let codigoEstudiante = prompt("ingrese apellido o codigo del estudiante a consultar")
-	console.log("el estudiante a consultado es",obtAlumnoporApellido(codigoEstudiante));
+main();
